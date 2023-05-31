9333e10fe42d99d6f5607d13855c31d8c5f6b6ae
MertArar <85193329+MertArar@users.noreply.github.com>
2023-05-31 19:19:51 +0300

    wrapper added to heroBanner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+import man from "../assets/man.png";
+import externalLinkIcon from "../assets/external-link-icon.png";
+import gmailIcon from "../assets/email-icon.png";
+import { scrollTo } from "../helper";
+import { useFollowPointer } from "./useFollowPointer";
 
 const HeroBanner = () => {
   return <div>HeroBanner</div>;
